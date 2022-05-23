cc590933d3628e6dc2fcca0d63edc3cc0608a835
HjalmarSnoep <26629952+HjalmarSnoep@users.noreply.github.com>
2022-05-23 15:58:04 +0200

    Add files via upload

---
+++ demo/demos.js
@@ -0,0 +1,29 @@
+var data=[
+  "forward playback",
+  "looped forward playback",
+  "pingpong playback",
+  "multiple-timelines",
+  "nested-timelines",
+  "multiple-svgs"
+  ];
+var menu=document.getElementById("menu");
+var header=document.createElement("div");
+header.id="header";
+document.body.appendChild(header);
+header.innerHTML="<big>MG-ASVG examples</big> <small>by Hjalmar Snoep</small>";
+console.log(menu);
+var ul=document.createElement("ul");
+menu.appendChild(ul);
+for(var i=0;i<data.length;i++)
+{
+  var li=document.createElement("li");
+  ul.appendChild(li);
+  var a=document.createElement("a");
+  a.innerHTML=data[i];
+  a.href=data[i].split(" ").join("-")+".html";
+  if(location.href.includes(a.href))
+  {
+    a.className="active";
+  }
+  li.appendChild(a);
+}
